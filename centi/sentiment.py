37c3614a84ea828f3952550fa3d43c2aec35dc84
Kit <kittsville@googlemail.com>
2016-10-31 21:32:04 +0000

    Switched to precompiled corpora

--- centi/sentiment.py
+++ centi/sentiment.py
@@ -6,11 +6,7 @@ centi.sentiment
 This module contains the methods for converting words into sentiment tuples
 """
 
-import os
-
-def abs_path(relative_path):
-    """Resolves relative path to absolute path"""
-    return os.path.join(os.path.dirname(__file__), relative_path)
+import corpus
 
 def get_sentiment(word):
     """Creates sentiment and modifier tuple for a word
@@ -18,24 +14,16 @@ def get_sentiment(word):
     Modifier can be pos (P), neg (N), invert (¬) or no modifier (=)
     """
     
-    sentiment = 0   # Default neutral sentiment
-    modifier  = '=' # Default no modifier
+    if word in corpus.positive:
+        sentiment = 1
+    elif word in corpus.negative:
+        sentiment = -1
+    else:
+        sentiment = 0
     
-    word += "\n"
-    
-    with open(abs_path('corpus/negative.txt'), 'r') as negativeWords:
-        for negWord in negativeWords:
-            if word == negWord:
-                sentiment = -1
-    
-    with open(abs_path('corpus/positive.txt'), 'r') as positiveWords:
-        for posWord in positiveWords:
-            if word == posWord:
-                sentiment = 1
-    
-    with open(abs_path('corpus/reverse.txt'), 'r') as reverseWords:
-        for revWord in reverseWords:
-            if word == revWord:
-                modifier = '¬'
+    if word in corpus.reverse:
+        modifier = '¬'
+    else:
+        modifier = '='
     
     return (sentiment, modifier)
