a7cb8e0f8c0a94c1fffd2c429fc58c635c7e6a64
ptravers <traverspw@gmail.com>
2016-11-02 20:20:03 +0000

    Added constants to offer improved readability

---
+++ centi/constants.py
@@ -0,0 +1,13 @@
+#Constants for sentiment determination
+
+#sentiment
+neutral = 0
+positive = 1
+mixed = 'M'
+negative = -1
+
+#tags
+tag_reverse = '~'
+tag_default = '='
+tag_positive = '+'
+tag_negative = '-'
