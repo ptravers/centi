b8b6dfa9d067defde3710e5706ef2e2ee3d8ea5f
Kit <kittsville@googlemail.com>
2016-11-06 15:20:07 +0000

    Clarified purpose of __init__.py

--- centi/__init__.py
+++ centi/__init__.py
@@ -7,7 +7,7 @@ The recursive Semtiment Analysis tool
 
 """
 
-# Import everything here you want available in the centi package
+# Import everything here you want available outside of the centi package
 from .parser import parse_sentence, parse_sentences
 from .sentiment import get_sentiment
 from .tree import get_polarity
